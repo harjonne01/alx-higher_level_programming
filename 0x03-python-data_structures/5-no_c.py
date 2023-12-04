b23a54a342c07559fd8cf597b821685bea3206f0
John Familoye <fajob20@yahoo.com>
2023-12-04 15:53:35 -0800

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,5 @@
 # function that removes all characters c and C from a string.
 
 def no_c(my_string):
-    New_str = ''
-    for chars in my_string:
-        if (chars != 'c' and chars != 'C'):
-            New_str += chars
-            return (New_str)
+    New_str = my_string.replace('c', '').replace('C', '')
+    return (New_str)
