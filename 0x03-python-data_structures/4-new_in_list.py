5d2a162502932fb3ac11a3c2b365adbb31b1d34f
John Familoye <fajob20@yahoo.com>
2023-12-04 15:40:30 -0800

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# function that replaces an element in a list at a specific position 
+# function that replaces an element in a list at a specific position
 # without modifying the original list
 
 def new_in_list(my_list, idx, element):
