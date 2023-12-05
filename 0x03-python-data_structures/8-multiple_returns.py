55ebd9a16eeb91739bf95e86b18768d23fd3346e
John Familoye <fajob20@yahoo.com>
2023-12-04 16:03:28 -0800

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# function that returns a tuple with the length of a string 
+# function that returns a tuple with the length of a string
 # and its first character.
 
 def multiple_returns(sentence):
